756258d7499765f4540e34e70f3962661c16d78b
Josh Peterson <peterson.joshua.j@gmail.com>
2017-10-29 21:37:10 -0400

    Updated models.py

--- fintechApp/app/models.py
+++ fintechApp/app/models.py
@@ -6,5 +6,17 @@ class CustomUser(models.Model):
 	last_name = models.CharField(max_length=50)
 	username = models.CharField(max_length=50)
 	password = models.CharField(max_length=50)
-	user_type = models.CharField(max_length=50)
+	user_type = models.ForeignKey(UserType)
 
+    def __str__(self):
+        return self.first_name + self.last_name
+    
+    def getType(self):
+        return self.user_type
+
+class UserType(models.Model):
+    title = models.CharField(max_length=50)
+    # permissions 
+
+    def __str__(self):
+        return self.title
