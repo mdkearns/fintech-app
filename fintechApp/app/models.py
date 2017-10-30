55ba7867064a2fa772a56f84d2707e87c3cb4ca0
Matt Kearns <mdk2mc@virginia.edu>
2017-10-29 21:26:29 -0400

    models

--- fintechApp/app/models.py
+++ fintechApp/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 
+class CustomUser(models.Model):
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	username = models.CharField(max_length=50)
+	password = models.CharField(max_length=50)
+	user_type = models.CharField(max_length=50)
+
