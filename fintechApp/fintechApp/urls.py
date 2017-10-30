5f847eabbf934710b19a5a19306b68fe0816f480
Josh Peterson <peterson.joshua.j@gmail.com>
2017-10-29 21:12:12 -0400

    Updated urlpatterns in urls.py and settings.py

--- fintechApp/fintechApp/urls.py
+++ fintechApp/fintechApp/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^app/', include('app.urls')),
+    url(r'^$', RedirectView.as_view(url='/app/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
