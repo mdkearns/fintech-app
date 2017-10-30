7aab3016b231da61d52afbfe1f85a3beda09262f
Kevin Wu <kjw2fa@virginia.edu>
2017-10-29 21:58:15 -0400

    added urls.py and views.py in apps folder

--- fintechApp/fintechApp/urls.py
+++ fintechApp/fintechApp/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
