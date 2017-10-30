addef02a8859fc08f2f0aadc8d2faa3e1af9bb9d
Kevin Wu <kjw2fa@virginia.edu>
2017-10-29 22:04:02 -0400

    Added urls and views to Apps

--- fintechApp/app/models.py
+++ fintechApp/app/models.py
@@ -1,22 +1,22 @@
 from django.db import models
 
 
-class CustomUser(models.Model):
-	first_name = models.CharField(max_length=50)
-	last_name = models.CharField(max_length=50)
-	username = models.CharField(max_length=50)
-	password = models.CharField(max_length=50)
-	user_type = models.ForeignKey(UserType)
-
-    def __str__(self):
-        return self.first_name + self.last_name
-    
-    def getType(self):
-        return self.user_type
-
 class UserType(models.Model):
     title = models.CharField(max_length=50)
-    # permissions 
+    # permissions
 
     def __str__(self):
         return self.title
+
+class CustomUser(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    user_type = models.ForeignKey(UserType)
+
+    def __str__(self):
+        return self.first_name + self.last_name
+
+    def getType(self):
+        return self.user_type
--- fintechApp/app/views.py
+++ fintechApp/app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import CustomUser
+
+def index(request):
+    """
+    View Function for home page of site
+    """
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(
+        request,
+        'templates/registration/login.html'
+    )
