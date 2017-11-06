8aa163a55f17e6aaf9fb26887abaefdb7811eb1d
Kevin Wu <kjw2fa@virginia.edu>
2017-11-06 03:28:23 -0500

    registered user made groups

--- fintechApp/app/admin.py
+++ fintechApp/app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import CustomUser, UserType, Report
+from .models import CustomUser, UserType, UserMadeGroup, Report
 
 admin.site.register(CustomUser)
 admin.site.register(UserType)
+admin.site.register(UserMadeGroup)
 admin.site.register(Report)
