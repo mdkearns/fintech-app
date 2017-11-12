b667d68d010825a9ae30c05af0444d021fc889a9
Ethan <ext8jv@virginia.edu>
2017-11-12 17:11:59 -0500

    removing old customuser and usertypes models

--- fintechApp/app/models.py
+++ fintechApp/app/models.py
@@ -5,31 +5,6 @@ from .choiceArrays import *
 from django.forms import ModelForm
 from django.urls import reverse
 
-
-class UserType(models.Model):
-    title = models.CharField(max_length=50)
-    # permissions
-
-    def __str__(self):
-        return self.title
-
-class CustomUser(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    user_type = models.ForeignKey(UserType)
-    is_company_user = models.BooleanField(default=False)
-    is_investor_user = models.BooleanField(default=False)
-    is_site_manager = models.BooleanField(default=False)
-
-
-    def __str__(self):
-        return self.first_name + self.last_name
-
-    def getType(self):
-        return self.user_type
-
 class UserMadeGroup(models.Model):
     """
     Model representing a user group. Can be created by any user.
