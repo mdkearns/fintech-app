44be77d562e4f72867ce94eb582e0a63f99614ac
Matt Kearns <mdk2mc@virginia.edu>
2017-12-04 14:06:16 -0500

    updating FDA command line application

--- fintechFDA/fintech_fda.py
+++ fintechFDA/fintech_fda.py
@@ -33,22 +33,45 @@ for report in reports:
 
 selection = input("\nEnter the name of the report to view (q to quit): ")
 
+last_report = None
+
 while True:
 	
 	#validate selected option 
 	
+	if selection == "s":
+		print("\nAvailable reports to view:\n")
+		for report in reports:
+			print("\t" + str(report))
+		selection = input("\nEnter the name of the next report to view (q to quit): ")
+	if selection == "d":
+		file_to_download = input("\nEnter the name of the file to download: ")
+		print("\nDownloaded file \"" + file_to_download + "\" for report: " + last_report)
+		selection = input("\nEnter the name of the next report to view (q to quit): ")
 	if selection == "q":
 		break
+		
+	last_report = selection
 	
 	if selection in reports:
 		login['report'] = selection
 		report_text = requests.get('http://127.0.0.1:8000/app/display_reports', params=login)
-		print()
+		file_text = requests.get('http://127.0.0.1:8000/app/get_report_files', params=login)
+		print("\nReport Details:\n")
 		print(report_text.text)
-		selection = input("Enter the name of the next report to view (q to quit): ")
+		print("\nAttached Files:\n")
+		print(file_text.text+ "\n")
+		selection = input("Please Enter one of the following:\n\ns : select next report\nd : download files for this report\nq : quit\n\n")
 	else:
 		print("\nYou selected: ", selection)
-		print("This report does not exist.")
-		selection = input("Specify the name of a valid report from the list above (q to quit): ")
+		print("This is an invalid command.")
+		selection = input("Please Enter one of the following:\n\ns : select next report\nd : download files for this report\nq : quit\n\n")
 
 print("Goodbye.")
+
+
+
+
+
+
+
